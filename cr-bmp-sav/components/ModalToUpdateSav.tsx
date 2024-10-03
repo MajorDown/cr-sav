48bc4fcd70bab1f8b50970d3cb04b7e9dcc15007
MajorDown <romain.fouillaron@gmx.fr>
2024-10-03 22:19:58 +0200

    implémentation du formulaire de mise à jour des SAV et du selecteur

--- cr-bmp-sav/components/ModalToUpdateSav.tsx
+++ cr-bmp-sav/components/ModalToUpdateSav.tsx
@@ -1,6 +1,8 @@
 import { SAV } from "@/constants/types"
+import Image from "next/image"
 import Modal from "./Modal"
 import { useState } from "react"
+import UpdateSAVInfosForm from "./UpdateSAVInfosForm"
 
 export type ModalToUpdateSavProps = {
     SAV: SAV
@@ -16,11 +18,20 @@ const ModalToUpdateSav = (props: ModalToUpdateSavProps) => {
                 <h3>SAV n°{props.SAV && props.SAV.id}</h3>
                 <p>Que souhaitez-vous faire ?</p>
                 <div id={'updateSelector'} >
-                    <button onClick={() => setUpdateChoice("updateLog")}>Actualiser le Log</button>
-                    <button onClick={() => setUpdateChoice("updateSAV")}>Modifier les infos</button>
+                    <button className={"updateBtn"} onClick={() => setUpdateChoice("updateLog")}>Actualiser le Log</button>
+                    <button className={"updateBtn"} onClick={() => setUpdateChoice("updateSAV")}>Modifier les infos</button>
+                </div>
+                <div id={"pointerSpace"}>
+                    <Image 
+                        className={updateChoice === "updateLog" ? "updateLog" : "updateSAV"} 
+                        src={"/images/triangle.png"} 
+                        alt={"pointer"}
+                        width={32} 
+                        height={24} 
+                    />
                 </div>
                 {updateChoice === "updateLog" && <p>Log</p>}
-                {updateChoice === "updateSAV" && <p>infos SAV</p>}
+                {updateChoice === "updateSAV" && <UpdateSAVInfosForm actualSav={props.SAV} />}
             </div>
         </Modal>
     )
